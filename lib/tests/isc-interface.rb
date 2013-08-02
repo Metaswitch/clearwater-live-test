22caca8b35fb609e2a5ced730929ba3ff024fad7
Rob Day <Robert.Day@metaswitch.com>
2013-08-02 13:35:49 +0100

    Revert "Temporarily excise B2BUA test"
    This reverts commit ebcd467a555406c8d21f704b991d31039ac1aba4.

--- lib/tests/isc-interface.rb
+++ lib/tests/isc-interface.rb
@@ -227,3 +227,80 @@ ASTestDefinition.new("ISC Interface - Redirect") do |t|
     sip_callee2.unregister
   )
 end
+
+
+=begin
+ASTestDefinition.new("ISC Interface - B2BUA") do |t|
+  sip_caller = t.add_sip_endpoint
+  sip_callee = t.add_sip_endpoint
+
+  sip_callee.set_ifc server_name: "#{ENV['HOSTNAME']}:5070"
+  
+  Thread.abort_on_exception = true
+  Thread.new do
+    c = TCPSIPConnection.new(5070)
+
+    incoming_cid = c.get_new_call_id
+    incoming_call = Call.new(c, incoming_cid)
+
+    incoming_call.recv_request("INVITE")
+    incoming_call.send_response("100")
+
+    # Send a new call back to Sprout
+    outgoing_call = Call.new(c, call_id: incoming_cid+"///2")
+    outgoing_call.setdest(incoming_call.get_next_hop_from_route, recv_from_this: true)
+
+    # Copy top Route header to Route or Request-URI?
+    outgoing_call.clone_details(incoming_call) # Copy To, From headers etc.
+    outgoing_call.send_request("INVITE")
+
+    outgoing_call.recv_response("180")
+    incoming_call.send_response("180")
+
+    outgoing_call.recv_response("200")
+
+    # Switch over to talking to Bono now the dialog is established
+    outgoing_call.setdest(outgoing_call.get_next_hop_from_rr)
+    outgoing_call.send_request("ACK")
+
+    incoming_call.send_response("200 OK", false, nil, {"Contact" => mock_as})
+    incoming_call.recv_request("ACK")  # Comes from Bono
+    incoming_call.recv_request("BYE")  # Also comes from Bono
+
+    # We automatically switch to Bono now, as that's where the BYE came from
+    incoming_call.send_response("200 OK")
+    incoming_call.end_call
+
+    outgoing_call.send_request("BYE")
+    outgoing_call.recv_response("200")
+    outgoing_call.end_call
+  end
+
+  t.set_scenario(
+    sip_caller.register +
+    sip_callee.register +
+    [
+      sip_caller.send("INVITE", target: sip_callee, emit_trusted: true),
+      sip_caller.recv("100"),
+      sip_callee.recv("INVITE", extract_uas_via: true),
+
+      sip_callee.send("180", target: sip_caller, method: "INVITE", cid_prefix: "2///"),
+      sip_caller.recv("180"),
+
+      sip_callee.send("200", target: sip_caller, method: "INVITE", cid_prefix: "2///"),
+      sip_callee.recv("ACK"),
+      sip_caller.recv("200", rrs: true),
+      sip_caller.send("ACK", target: sip_callee, in_dialog: true, method: "INVITE"),
+
+      SIPpPhase.new("pause", sip_caller, timeout: 1000),
+      sip_caller.send("BYE", target: sip_callee, in_dialog: true),
+      sip_caller.recv("200"),
+      sip_callee.recv("BYE"),
+      sip_callee.send("200", target: sip_callee, method: "BYE", cid_prefix: "2///"),
+    ] +
+    sip_caller.unregister +
+    sip_callee.unregister
+  )
+end
+
+=end
