2db08a00fea8eab9e59f48047809a743807e5389
Rob Day <Robert.Day@metaswitch.com>
2013-10-22 12:57:28 +0100

    Use SNMP_AGENTS environment variable for more control over SNMP targets

--- lib/tests/snmp.rb
+++ lib/tests/snmp.rb
@@ -12,43 +12,44 @@ def get_snmp host, community, oid
 end
 
 
-servers = ["bono-1", "sprout-1"]
+servers = ENV['SNMP_AGENTS'].split(",")
 
 servers.each do |s|
   NonCallTestDefinition.new("SNMP - #{s}") do |domain, t|
-    host = "#{s}.#{domain}"
+    host = s
     snmp_map = get_snmp host, "clearwater", "1.2.826.0.1.1578918.9.2"
     if (snmp_map.empty?)
-      raise "No results from SNMP for host #{s}.#{domain}"
+      raise "No results from SNMP for host #{host}"
     end
     true
   end
 
   NonCallTestDefinition.new("CPU/Mem SNMP - #{s}") do |domain, t|
-    host = "#{s}.#{domain}"
+    host = s
     snmp_map = get_snmp host, "clearwater", "1.3.6.1.4.1.2021"
     if (snmp_map.empty?)
-      raise "No results from SNMP for host #{s}.#{domain}"
+      raise "No results from SNMP for host #{host}"
     end
     true
   end
 
   NonCallTestDefinition.new("Nonexistent SNMP - #{s}") do |domain, t|
-    host = "#{s}.#{domain}"
+    host = s
     oid = "1.2.826.0.1.1578918.9.2.99999"
     snmp_map = get_snmp host, "clearwater", oid
     if (not snmp_map.empty?)
       raise "Got unexpected result from SNMP #{oid} for host
-                                #{s}.#{domain} - #{snmp_map.inspect}"
+                                #{host} - #{snmp_map.inspect}"
     end
     true
   end
 
   NonCallTestDefinition.new("SNMP with wrong community - #{s}") do |domain, t|
-    host = "#{s}.#{domain}"
+    host = s
+
     begin
       snmp_map = Timeout::timeout(2)  { get_snmp host, "public", "1.2.826.0.1.1578918.9.2" }
-      raise "Got results from SNMP for host #{s}.#{domain} even with an incorrect community string"
+      raise "Got results from SNMP for host #{host} even with an incorrect community string"
     rescue TimeoutError
       true
     end
