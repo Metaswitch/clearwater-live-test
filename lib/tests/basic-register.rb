348b7db303a7ee09742e94b338f56f9fa5f9f36e
Rob Day <Robert.Day@metaswitch.com>
2014-01-13 11:15:35 +0000

    Use Authorization header on initial REGISTER to convey private ID

--- lib/tests/basic-register.rb
+++ lib/tests/basic-register.rb
@@ -36,7 +36,13 @@ TestDefinition.new("Basic Registration") do |t|
   caller = t.add_endpoint
 
   t.add_quaff_scenario do
-    caller.register
+    call = caller.outgoing_call(caller.uri)
+    call.send_request("REGISTER", "", { "Expires" => "3600", "Authorization" => %Q!Digest username="#{caller.private_id}"! })
+    response_data = call.recv_response("401")
+    auth_hdr = Quaff::Auth.gen_auth_header response_data.header("WWW-Authenticate"), caller.private_id, caller.password, "REGISTER", caller.uri
+    call.update_branch
+    call.send_request("REGISTER", "", {"Authorization" => auth_hdr, "Expires" => expires.to_s, "CSeq" => "2 REGISTER"})
+    response_data = call.recv_response("200")
   end
 
   t.add_quaff_cleanup do
@@ -50,8 +56,21 @@ TestDefinition.new("Multiple Identities") do |t|
   ep2 = t.add_quaff_public_identity(ep1)
 
   t.add_quaff_scenario do
-    ok = ep1.register
-    ok2 = ep2.register
+    call = ep1.outgoing_call(ep1.uri)
+    call.send_request("REGISTER", "", { "Expires" => "3600", "Authorization" => %Q!Digest username="#{ep1.private_id}"! })
+    response_data = call.recv_response("401")
+    auth_hdr = Quaff::Auth.gen_auth_header response_data.header("WWW-Authenticate"), ep1.private_id, ep1.password, "REGISTER", ep1.uri
+    call.new_transaction
+    call.send_request("REGISTER", "", {"Authorization" => auth_hdr, "Expires" => "3600"})
+    ok = call.recv_response("200")
+
+    call2 = ep2.outgoing_call(ep2.uri)
+    call2.send_request("REGISTER", "", { "Expires" => "3600", "Authorization" => %Q!Digest username="#{ep2.private_id}"! })
+    response_data = call2.recv_response("401")
+    auth_hdr = Quaff::Auth.gen_auth_header response_data.header("WWW-Authenticate"), ep2.private_id, ep2.password, "REGISTER", ep1.uri
+    call2.new_transaction
+    call2.send_request("REGISTER", "", {"Authorization" => auth_hdr, "Expires" => "3600"})
+    ok2 = call2.recv_response("200")
 
     fail "200 OK for #{ep1.uri} does not include <#{ep1.uri}>" unless ok.all_headers("P-Associated-URI").include? "<#{ep1.uri}>"
     fail "200 OK for #{ep1.uri} does not include <#{ep2.uri}>" unless ok.all_headers("P-Associated-URI").include? "<#{ep2.uri}>"
