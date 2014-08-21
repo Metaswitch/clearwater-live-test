d07b23afe58566d8e0814bdf62090a5d0ad68bba
Rob Day <Robert.Day@metaswitch.com>
2014-08-21 13:04:10 +0100

    Update skipping of SUBSCRIBE test

--- lib/tests/subscribe.rb
+++ lib/tests/subscribe.rb
@@ -32,7 +32,8 @@
 # under which the OpenSSL Project distributes the OpenSSL toolkit software,
 # as those licenses appear in the file LICENSE-OPENSSL.
 
-SkippedTestDefinition.new("SIP SUBSCRIBE-NOTIFY") do |t|
+TestDefinition.new("SIP SUBSCRIBE-NOTIFY") do |t|
+  t.skip
 
   ep1 = t.add_endpoint
   t.add_quaff_setup do
