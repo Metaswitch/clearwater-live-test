f34ea6bd366839b52f338061ead86a6d9bdfebfb
Rob Day <Robert.Day@metaswitch.com>
2014-09-01 18:44:49 +0100

    Further fixes from testing

--- lib/endpoint.rb
+++ lib/endpoint.rb
@@ -36,7 +36,7 @@ require 'forwarder'
 
 class Endpoint
   extend Forwarder
-  forward_all :password, :sip_uri, :domain, :private_id, :pstn, :transport, :set_simservs, :set_ifc, :domain, :instance_id, to: :line_info
+  forward_all :password, :username, :sip_uri, :domain, :private_id, :pstn, :transport, :set_simservs, :set_ifc, :domain, :instance_id, to: :line_info
   attr_reader :line_info, :transport
 
   def element_type
