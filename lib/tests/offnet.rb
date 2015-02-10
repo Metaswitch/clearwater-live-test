e29443d1a55294c38a5b24f202bcf6b8e7ff0a81
Rob Day <Robert.Day@metaswitch.com>
2015-02-10 18:40:18 +0000

    Add optional live tests to exercise arbitrary BGCF routing function (domain-based or rn-based)

---
+++ lib/tests/offnet.rb
@@ -0,0 +1,68 @@
+TestDefinition.new("Off-net calls - tel: URI") do |t|
+  t.skip_unless_offnet_tel
+
+  caller = t.add_endpoint
+  as = t.add_as 5072
+
+
+  t.add_quaff_setup do
+    caller.register
+  end
+
+  t.add_quaff_scenario do
+    call = caller.outgoing_call("tel:#{ENV['OFF_NET_TEL']}")
+
+    call.send_request("MESSAGE", "hello world\r\n",
+                      {"Content-Type" => "text/plain"})
+    call.recv_response("200")
+    call.end_call
+  end
+
+  t.add_quaff_scenario do
+    call2 = as.incoming_call
+
+    call2.recv_request("MESSAGE")
+    call2.send_response("200", "OK")
+    call2.end_call
+  end
+
+  t.add_quaff_cleanup do
+    caller.unregister
+  end
+
+end
+
+TestDefinition.new("Off-net calls - sip: URI") do |t|
+  t.skip_unless_offnet_tel
+
+  caller = t.add_endpoint
+  as = t.add_as 5072
+
+
+  t.add_quaff_setup do
+    caller.register
+  end
+
+  t.add_quaff_scenario do
+    call = caller.outgoing_call("sip:#{ENV['OFF_NET_TEL']}@#{t.deployment};user=phone")
+
+    call.send_request("MESSAGE", "hello world\r\n",
+                      {"Content-Type" => "text/plain"})
+    call.recv_response("200")
+    call.end_call
+  end
+
+  t.add_quaff_scenario do
+    call2 = as.incoming_call
+
+    call2.recv_request("MESSAGE")
+    call2.send_response("200", "OK")
+    call2.end_call
+  end
+
+  t.add_quaff_cleanup do
+    caller.unregister
+  end
+
+end
+
