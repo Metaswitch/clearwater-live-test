48abf6f5e6fcd4e0c049749098867dade1fd9317
Andy Caldwell <andrew.caldwell@metaswitch.com>
2015-04-15 01:25:39 +0000

    Alarm support

--- lib/daemon.rb
+++ lib/daemon.rb
@@ -2,20 +2,47 @@
 
 require 'daemons'
 require 'pathname'
+require 'ffi-rzmq'
+require 'syslog'
 require_relative './live-test'
 
 def source(file, vars)
   ENV.replace(eval(`/bin/bash -c 'source #{file} && export #{vars.join " "} && ruby -e "p ENV"'`))
 end
 
+def raise_alarm(id)
+  context = ZMQ::Context.new
+  client = context.socket(ZMQ::REQ)
+  client.connect("tcp://127.0.0.1:6664")
+
+  poller = ZMQ::Poller.new
+  poller.register(client, ZMQ::POLLIN)
+
+  client.send_strings ["issue-alarm", "clearwater-live-verification", id]
+
+  poller.poll(2000)
+
+  if poller.readables.include? client
+    response = []
+    client.recv_strings(response)
+  else
+    Syslog.err("Dropped alarm #{id}")
+  end
+
+  client.close
+end
+
 path = Pathname.new("/var/log/clearwater-live-verification/");
 Daemons.run_proc("clearwater-live-verification",
                  dir_mode: :normal,
                  dir: path) do
-  system("echo Clearing alarm")
+  # Initially we don't know the state of the deployment
+  raise_alarm("6000.2")
+
   loop do
+
     # Sort out the appropriate ENV variables from clearwater config.
-    source("/etc/clearwater/config", ["home_domain", "ellis_address", "pcscf_address"])
+    source("/etc/clearwater/config", ["home_domain", "ellis_address", "pcscf_address", "signup_key"])
     if ENV['ellis_hostname'] then
       ENV['ELLIS'] = ENV['ellis_hostname']
     else
@@ -26,6 +53,7 @@ Daemons.run_proc("clearwater-live-verification",
     else
       ENV['PROXY'] = "#{ENV['home_domain']}"
     end
+    ENV['SIGNUP_CODE'] = ENV['signup_key']
     ENV['TRANSPORT'] = 'tcp'
 
     # These tests exit with their success code so catch the exit status here.
@@ -37,10 +65,13 @@ Daemons.run_proc("clearwater-live-verification",
 
     if success == 0 then
       # Tests passed - clear the alarm
-      system("echo Clearing alarm")
+      raise_alarm("6000.1")
     else
-      # Tests failed - raise the alarm and collect diags
-      system("echo Raising alarm")
+      # Tests failed - raise the critical alarm
+      raise_alarm("6000.3")
+
+      # Sleep for a bit (failing the test can happen very fast and we don't
+      # want to DoS the deployment.
       sleep 30
     end
   end
